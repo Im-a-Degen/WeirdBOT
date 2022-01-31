223a501ba374028d72709b58e5b312defe84eeed
Im-a-Degen <58045054+Im-a-Degen@users.noreply.github.com>
2022-01-31 19:25:52 +0000

    separated input data from main program.

--- WeirdBOT.py
+++ WeirdBOT.py
@@ -1,8 +1,8 @@
 import discord
+from BOTrequiredData import *
 
 client = discord.Client()
-recipient = "Im A Degen#4444" #"becca#9670"
-emote_weird = "botWEIRD:937752793886912512"
+
 
 @client.event
 async def on_ready():
@@ -17,11 +17,11 @@ async def on_message(message):
 
     if message.author == recipient:
         print("yes")
-        await message.add_reaction(":botWEIRD:")
+        await message.add_reaction(emote_weird)
 
     if message.content == "test":
         print("yes")
         await message.add_reaction(emote_weird)
 
 
-client.run("OTM3NzUzNDM1OTQ2Nzc0NTU4.YfgU8Q.V_MphgmHMnqavECAb2nKIk4NE20")
+client.run(bot_key)
