e667641316a4d7f1e300a8d8de97144a79251aa1
Im-a-Degen <58045054+Im-a-Degen@users.noreply.github.com>
2022-01-31 19:34:03 +0000

    :YEP:

---
+++ BOTrequiredDataPublic.py
@@ -0,0 +1,10 @@
+# data for WeirdBOT.py
+# recipients
+weird_recipient = ""
+
+# emotes
+emote_weird = "botWEIRD:937752793886912512"
+emote_pogD = "PogD:869956696037031977"
+
+# key for auth
+bot_key = ""
