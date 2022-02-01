510fdf1777accb460d0feb372a050d771467fc56
Im-a-Degen <58045054+Im-a-Degen@users.noreply.github.com>
2022-02-01 21:57:52 +0000

    PogD works + CD + Weird + Ratio + L

--- WeirdBOT.py
+++ WeirdBOT.py
@@ -13,20 +13,15 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.author != client.user:
+        global pogD_token
 
         if str(message.author) == weird_recipient:
-            print("yes")
             await message.add_reaction(emote_weird)
 
-        if message.content == emote_pogD:
-            print("pogD detected")
-            global pogD_token
-            if pogD_token:
-                print("pogD time")
-                pogD_token = False
-                await message.channel.send("<:botPogD:937903199698952233>")
+        if pogD_token and message.content == emote_pogD:
+            pogD_token = False
+            await message.channel.send("<:botPogD:937903199698952233>")
         else:
             pogD_token = True
-
 
 client.run(bot_key)
