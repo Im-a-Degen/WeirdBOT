389ec66f8d4c9d4f4cecb648bb3c9b5f5b00fe54
Im-a-Degen <58045054+Im-a-Degen@users.noreply.github.com>
2022-02-01 03:01:03 +0000

    PogD works + CD + Weird + Ratio

--- WeirdBOT.py
+++ WeirdBOT.py
@@ -2,6 +2,7 @@ import discord
 from BOTrequiredDataPrivate import *
 
 client = discord.Client()
+pogD_token = True
 
 
 @client.event
@@ -16,6 +17,16 @@ async def on_message(message):
         if str(message.author) == weird_recipient:
             print("yes")
             await message.add_reaction(emote_weird)
+
+        if message.content == emote_pogD:
+            print("pogD detected")
+            global pogD_token
+            if pogD_token:
+                print("pogD time")
+                pogD_token = False
+                await message.channel.send("<:botPogD:937903199698952233>")
+        else:
+            pogD_token = True
 
 
 client.run(bot_key)
